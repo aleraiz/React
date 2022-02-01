cc3beb33afd907098b2246604e65a5086a816367
aleraiz <alejandroraiz7@gmail.com>
2022-01-31 23:37:29 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let contador = 1;
+  contador++
+  console.log(contador);
+
+  let edad = 18;
+
+  // if(edad>=18){
+  //   console.log("es mayor de edad");
+  // } else {
+  //   console.log("es menor de edad");
+  // }
+
+  // se puede escribir de la siguiente manera:
+  edad >= 18 ? console.log("es mayor de edad") : console.log("es menor de edad");
+
+
   return (
     <div className="App">
       <header className="App-header">
