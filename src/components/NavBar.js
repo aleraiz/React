05d9dea3babaeba768bebcfdff40ed3b53dc67d8
aleraiz <alejandroraiz7@gmail.com>
2022-02-02 19:36:15 -0300

    commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css'
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className='Lista-header'>
+        <li>Home</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
