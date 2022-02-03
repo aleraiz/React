e7b271b309da364c60cade1ae47d0b0defd421df
aleraiz <alejandroraiz7@gmail.com>
2022-02-03 19:48:14 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ function App() {
   return (
     <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <NavBar/>
-            <div className='Menu-header'>
-              <ul className='Lista-header'>
-                <li>Favoritos</li>
-                <li>Cuenta</li>
-                <li>Bolsa</li>
-              </ul>
-            </div>
           </header>   
   </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import '../App.css'
+import logo from '../logo.svg'
 
 function Navbar() {
   return (
     <nav>
-      <ul className='Lista-header'>
-        <li>Home</li>
-        <li>Productos</li>
-        <li>Contacto</li>
-      </ul>
+      <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <div>
+        <ul className='Lista-header'>
+          <li><a href="">Home</a></li>
+          <li><a href="">Productos</a></li>
+          <li><a href="">Contacto</a></li>
+        </ul>
+      </div>
+      <div className='Menu-header'>
+              <ul className='Lista-header'>
+                <li><i class="usuario__iconos fas fa-heart" aria-hidden="true"></i></li>
+                  <li><i class="usuario__iconos fas fa-user" aria-hidden="true"></i></li>
+                  <li><i class="usuario__iconos fas fa-shopping-bag" aria-hidden="true"></i></li>
+              </ul>
+      </div>
+
     </nav>
   );
 }
